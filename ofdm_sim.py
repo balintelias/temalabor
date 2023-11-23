67350b446497fdbf3f684571530f48e7a7becba3
balintelias <ebalint896@gmail.com>
2023-11-23 15:20:44 +0100

    refactored and simulated outputs for consultation final changes in ofdm_sim.py

--- ofdm_sim.py
+++ ofdm_sim.py
@@ -1,29 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import special
+import math
 
 
-def theoreticalBER(EbNo):
-    return 0.5 * special.erfc(np.sqrt(EbNo))
+def theoreticalBER(EbNodB):
+    return 0.5 * special.erfc(np.sqrt(pow(10, EbNodB / 10)))
 
 
 def calculateBITno(theoreticalBER):
     return pow(10, -np.log10(theoreticalBER) + 2)
 
 
-def generateBITs(bitNo):
-    bitnumber = bitNo
+def calculateSymbolNo(bitNo, mu):
+    return math.ceil(bitNo / mu)
+
+
+def calculateOFDMsymbolNo(symbolNo, carrierNo):
+    return math.ceil(symbolNo / carrierNo)
+
+
+def generateBITs(bitNo, carrierNo, mu):
+    bitnumber = math.ceil(math.ceil(bitNo / mu) / carrierNo) * carrierNo * mu
     bits = np.random.binomial(n=1, p=0.5, size=bitnumber)
     return bits
 
 
-# S/P átalakító, bitcsoportokat csinál a bitsorozatból
 def SP(bits):
-    bits_SP = [(bits[i], bits[i + 1]) for i in range(0, len(bits), 2)]
+    bits_SP = bits.reshape(-1, 2)
     return bits_SP
 
 
-# QPSK szimbólumokat képez a bitpárokból
 def mapping(bits_SP):
     mapping_table = {
         (0, 0): -1 - 1j,
@@ -31,23 +38,36 @@ def mapping(bits_SP):
         (1, 0): +1 - 1j,
         (1, 1): +1 + 1j,
     }
-    symbols = [mapping_table[pair] for pair in bits_SP]
+    symbols = np.array([mapping_table[tuple(pair)] for pair in bits_SP])
     return symbols
 
 
-# Inverz FFT:
-# A frekvenciatartományban lévő szimbólumokból időtartománybeli jelet képez
-def IFFT(symbols):
-    return np.fft.ifft(symbols)
+def IFFT(symbols_frequency):
+    num_rows, num_cols = symbols_frequency.shape
+    ifft_results = np.empty_like(symbols_frequency, dtype=complex)
+
+    for i in range(num_rows):
+        subarray = symbols_frequency[i]  # Access each subarray
+        ifft_result = np.fft.ifft(subarray)  # Apply IFFT to the subarray
+        ifft_results[i] = ifft_result
+    signal_time = ifft_results
+    return signal_time
 
 
-def addCP(symbol_time, CP=10):
-    prefix = symbol_time[-CP:]
-    return np.concatenate([prefix, symbol_time])
+def addCP(signal_time, CP=10):
+    num_rows, num_cols = signal_time.shape
+    signal_time_withCP = np.empty((num_rows, num_cols + CP), dtype=complex)
+
+    for i in range(num_rows):
+        prefix = np.array(signal_time[i, -CP:])
+        type1 = type(prefix)
+        type2 = type(signal_time[i])
+        type3 = type(signal_time_withCP)
+        signal_time_withCP[i] = np.concatenate((prefix, signal_time[i]))
+
+    return signal_time_withCP
 
 
-# Átviteli csatorna
-# megadott teljesítményű addítív fehér Gauss-zajjal
 def channel(signal, SNRdB, channelResponse):
     # konvolúció
     output_signal = np.convolve(signal, channelResponse, mode="same")
@@ -58,18 +78,52 @@ def channel(signal, SNRdB, channelResponse):
     noise_real = np.sqrt(sigma2) / 2 * np.random.randn(*output_signal.shape)
     noise_imaginary = np.sqrt(sigma2) / 2 * 1j * np.random.randn(*output_signal.shape)
     noise = noise_real + noise_imaginary
+    # return output_signal
     return output_signal + noise
 
+def equalize(signal, channelResponse):
+    response_frequency = np.fft.fft(channelResponse)
+    inverse_response = np.fft.ifft(1 / response_frequency)
+    output_signal = np.convolve(signal, inverse_response, mode="same")
+    return output_signal
 
-def removeCP(symbol_time, CP=10):
-    return symbol_time[CP:]
+
+def removeCP(signal_time_withCP, CP=10):
+    num_rows, num_cols = signal_time_withCP.shape
+    signal_time = np.empty((num_rows, num_cols - CP), dtype=complex)
+
+    for i in range(num_rows):
+        signal_time[i] = np.array(signal_time_withCP[i, CP:])
+    return signal_time
 
 
-def FFT(ofdm_parallel):
-    return np.fft.fft(ofdm_parallel)
+def FFT(signal_time):
+    num_rows, num_cols = signal_time.shape
+    fft_results = np.empty_like(signal_time, dtype=complex)
+
+    for i in range(num_rows):
+        subarray = signal_time[i]  # Access each subarray
+        fft_result = np.fft.fft(subarray)  # Apply FFT to the subarray
+        fft_results[i] = fft_result
+    # signal_time = fft_results
+    return fft_results
 
 
 def demapping(QAM):
+    num_rows, num_cols = QAM.shape
+
+    demapped_bits = np.empty((num_rows, num_cols * 2), dtype=int)
+    for i in range(num_rows):
+        returned = demap_one(QAM[i]).reshape(-1)
+        shape1 = returned.shape
+        shape2 = demapped_bits[i].shape
+        demapped_bits[i] = returned
+    return np.array(demapped_bits).reshape(-1, 2)
+
+def demap_one(symbol):
+    # print("QAM", symbol)
+    # print("QAM type", type(symbol))
+
     demapping_table = {
         (-1 - 1j): (0, 0),
         (-1 + 1j): (0, 1),
@@ -81,7 +135,7 @@ def demapping(QAM):
 
     # a legközelebbi pontokhoz tartozó távolságok megkaphatók a komplex számok
     # különbségeként ami vektorokként szemléletes
-    diff_vectors = QAM.reshape((-1, 1)) - constellation.reshape((1, -1))
+    diff_vectors = symbol.reshape((-1, 1)) - constellation.reshape((1, -1))
     dists = abs(diff_vectors)
 
     # megkeressük a legkisebb távolságok indexeit
@@ -94,103 +148,59 @@ def demapping(QAM):
     received = np.vstack([demapping_table[C] for C in decision])
     return received
 
-
-# bit
 def PS(bits_parallel):
     # bits = [b for a in bits_parallel for b in a]
     return bits_parallel.reshape((-1,))
 
-
-# ber
-def calculate_BER(OFDM_demapped, bits):
+def calculateBER(bits, bits_received):
     error = 0
-    OFDM_1D = np.concatenate(OFDM_demapped)
     array_sent = np.array(bits)
-    array_received = np.array(OFDM_1D)
+    array_received = np.array(bits_received)
     for i in range(len(array_sent)):
         if array_sent[i] != array_received[i]:
             error += 1
     return error / len(bits)
 
 
-K = 128  # vivők száma
-P = 0  # pilot vivő
-mu = 2  # 2 bit/szimbólum
-carrierNo = K - P
-CP = 10  # cyclic prefix hossza
+carrierNo = 128
+mu = 2
+# channelResponse = np.array([1])
+channelResponse = np.array([math.sqrt(9), math.sqrt(1)])
+data_pairs = []
 
-simulated_BER = []
-
-for x in range(11):  # for simulation
-    # for x in range(6): # for testing
+for x in range(11):
     SNRdB = x
-    payloadBits_per_OFDM = mu * (K - P)
-    bitsToSimulate = calculateBITno(theoreticalBER(SNRdB))
-    numberOfSymbols = int(bitsToSimulate / payloadBits_per_OFDM + 10)
+    calculated_bitNo = calculateBITno(theoreticalBER(SNRdB))
+    OFDMsymbolNo = calculateOFDMsymbolNo(
+        calculateSymbolNo(calculated_bitNo, mu), carrierNo
+    )
+    bits = generateBITs(calculated_bitNo, carrierNo, mu)
+    bits_SP = SP(bits)
+    symbols = mapping(bits_SP)
+    symbols_frequency = symbols.reshape(-1, carrierNo)
+    signal_time = IFFT(symbols_frequency)
+    signal_time_withCP = addCP(signal_time)
+    OFDM_tx = signal_time_withCP.reshape(-1)
+    OFDM_rx = channel(OFDM_tx, SNRdB, channelResponse)
+    OFDM_equalized = equalize(OFDM_rx, channelResponse)
+    # OFDM_equalized = OFDM_rx
+    OFDM_equalized = OFDM_equalized.reshape(-1, carrierNo + 10)
+    OFDM_noCP = removeCP(OFDM_equalized)
+    OFDM_frequency = FFT(OFDM_noCP)
+    OFDM_demapped = demapping(OFDM_frequency)
+    bits_received = PS(OFDM_demapped)
+    BitErrorRate = calculateBER(bits, bits_received)
+    data_pairs.append((SNRdB, BitErrorRate))
+    print("SNRdB: ", SNRdB, "Bit Error Rate: ", BitErrorRate)
 
-    channelResponse = np.array([np.sqrt(0.6), np.sqrt(0.3), np.sqrt(0.1)]) # négyzetösszeg legyen
-    # channelResponse = np.array([1])  # for testing
-
-    pairs = []
-    signal_time = []
-
-    # a generálást egészen addig amíg időtartományba nem transzformálunk, továbbra is for ciklussal
-    ofdm_time = []
-    bits_sum = []
-    for symbol in range(numberOfSymbols):
-        bits = generateBITs(payloadBits_per_OFDM)  # egy OFDM adás bitjei
-        bits_SP = SP(bits)  # bitek párokba rendezve
-        symbols = mapping(bits_SP)  # 128 minta frekvenciatartományban
-        symbol_time = IFFT(symbols)  # 128 minta időtartományban
-        len_symbol_time = len(symbol_time)
-        symbol_time_withCP = addCP(symbol_time, CP)  # 10 + 128 minta időtartományban
-        len_symbol_time_withCP = len(symbol_time_withCP)
-        ofdm_time.extend(
-            symbol_time_withCP
-        )  # egész időtartománybeli jelforma N * (10 + 128 minta)
-        len_ofdm_time = len(ofdm_time)
-        bits_sum.extend(bits)  # összes bit N * 128 bit
-    ofdm_received = channel(ofdm_time, SNRdB, channelResponse)  # N * (10 + 128) minta
-    len_ofdm_received = len(ofdm_received)
-    ofdm_rec_reshaped = ofdm_received.reshape(
-        (-1, (K + CP))
-    )  # szimbólumonként sorokra osztani N x (10 + 128) 
-    dim = ofdm_received.shape
-    received_symbols_noCP = [
-        removeCP(symbol_withCP) for symbol_withCP in ofdm_rec_reshaped
-    ]
-    # received_symbols_noCP = [removeCP(symbol_withCP, CP) for symbol_withCP in received_symbols_withCP]
-    received_symbols_freq = [FFT(symbol) for symbol in received_symbols_noCP]
-    demapped = [demapping(symbol_freq) for symbol_freq in received_symbols_freq]
-    bits_received = [PS(demapped_symbol) for demapped_symbol in demapped]
-    calculated_BER = calculate_BER(bits_received, bits_sum)
-    print("SNR: ", SNRdB, " Obtained Bit error rate: ", calculated_BER)
-    simulated_BER.append((x, calculated_BER))
-
-print(simulated_BER)
-plt.plot(*zip(*simulated_BER))
+print(data_pairs)
+plt.plot(*zip(*data_pairs))
 plt.semilogy()
 plt.xlabel("SNR (in dB)")
 plt.ylabel("Bit Error Rate")
 plt.title("Bit Error Rate Simulation of an OFDM System with QPSK modulation")
+plt.suptitle("Frequency Dependent Channel, Equalized")
 plt.grid(True, linestyle="--", linewidth=0.5, color="gray", alpha=0.5)
-plt.savefig("simulatedBER_sajat.png", bbox_inches="tight")
+plt.savefig("simulatedBER_equalized.png", bbox_inches="tight")
 plt.show()
 plt.close()
-
-# TODO:
-# FIXME:
-
-# IMSC pontokért vektorizált mátrixos _izé_
-
-# TODO:
-# CP megírása:
-# CP hossza > h hossza (csatorna impulzusválasza)
-# ehhez kell:
-# addCP az IFFT után
-#  és
-# removeCP az FFT előtt
-# függvények
-
-# TODO:
-# H ismeretében H inverzével csatornakompenzálás
